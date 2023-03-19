d61de3e3437be7397853cf2bc2bd9e39012a0d4c
Kumod Sharma <123074798+kumod007@users.noreply.github.com>
2023-03-19 12:13:20 +0530

    Add files via upload

---
+++ SQl/class 1.sql
@@ -0,0 +1,48 @@
+#Arithmetic Functions
+
+#Addition
+
+select 20+20;
+
+#Subtraction
+
+select 20-20;
+
+#Multiplication
+
+select 20*20;
+
+#Division
+
+select 20/2;
+
+#Modulus
+
+select 21%2;
+
+#Floor & Ceil
+
+select floor(20.45);
+
+select ceil(20.22);
+
+#Round
+
+select round(20.2);
+select round(30.8);
+
+select round(20.246,2);
+
+select round(21.25,1);
+
+
+#Random Functions
+
+select rand();
+
+select rand()*100;
+
+select round(20 + rand()*100);
+
+
+
